598bae71aec71ea8b5904e8b1f28abfe231fbfba
Kate <mitflash56@gmail.com>
2023-08-16 19:13:18 +0300

    added types to post component

--- components/post.tsx
+++ components/post.tsx
@@ -1,6 +1,7 @@
-import Image from "next/image";
+import Image from "next/image"
 import React from 'react'
-export default function Post({src, text}) {
+
+export default function Post({src, text}:{src:string, text:string}) {
     
     return (
         <div className="w-[100%] bg-white rounded mb-5 p-5 flex flex-col gap-3 justify-center items-center">
